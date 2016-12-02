65da82aadcbd64c1baf42aa9dec58f4e46b42422
Caroline Siqueira <carolinesiqueira@iMac.local>
2016-12-02 15:43:56 -0200

    new file to json

--- localinfos.js
+++ localinfos.js
@@ -4,5 +4,9 @@
     {"id":"2","title":"Posto da UFSM" , "subtitle":"conveniências", "latitude":"-29.711385","longitude":"-53.715995" , "cardapio":"salgados\ncachorro-quente\n","aval":"3" },
     {"id":"3","title":"Xis do HUSM" , "subtitle":"lanches e almoços", "latitude":"-29.713931","longitude":"-53.714774" , "cardapio":"xis completo\nxis salada\n xis frango", "aval":"4" },
     {"id":"4","title":"Lancheria do CCNE" , "subtitle":"lanches", "latitude":"-29.714625","longitude":"-53.716606" , "cardapio":"salgados\npão de queijo <3", "aval":"5" },	
-    {"id":"5","title":"Restaurante Universitário" , "subtitle":"refeições: café da manhã, almoço e janta!", "latitude":"-29.714113","longitude":"-53.718439" , "cardapio":"conferir no portal", "aval":"5" },	
+    {"id":"5","title":"Restaurante Universitário" , "subtitle":"refeições: café da manhã, almoço e janta!", "latitude":"-29.714113","longitude":"-53.718439" , "cardapio":"conferir no portal da UFSM", "aval":"5" },
+    {"id":"6","title":"Restaurante da Cléo" , "subtitle":"lanches e almoço", "latitude":"-29.722620","longitude":"-53.718108" , "cardapio":"Almoço\npasteis\nlanches", "aval":"4" },
 ],}
+
+
+
