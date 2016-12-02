9dba496d349560ab322544dc1e3c7a7d17ad3ace
Caroline Siqueira <carolinesiqueira@iMac.local>
2016-12-02 15:39:46 -0200

    new file to json

--- localinfos.js
+++ localinfos.js
@@ -3,5 +3,6 @@
     {"id":"1","title":"Lancheria do Menezes" , "subtitle":"lanches e almoço", "latitude":"-29.712860","longitude":"-53.716847" , "cardapio":"salgados gourmet\nsanduíches gourmet\ncoisas caras", "aval":"2" },
     {"id":"2","title":"Posto da UFSM" , "subtitle":"conveniências", "latitude":"-29.711385","longitude":"-53.715995" , "cardapio":"salgados\ncachorro-quente\n","aval":"3" },
     {"id":"3","title":"Xis do HUSM" , "subtitle":"lanches e almoços", "latitude":"-29.713931","longitude":"-53.714774" , "cardapio":"xis completo\nxis salada\n xis frango", "aval":"4" },
-    {"id":"4","title":"Lancheria do CCNE" , "subtitle":"lanches", "latitude":"-29.715718","longitude":"-53.715030" , "cardapio":"salgados\npão de queijo <3", "aval":"5" },	
+    {"id":"4","title":"Lancheria do CCNE" , "subtitle":"lanches", "latitude":"-29.714625","longitude":"-53.716606" , "cardapio":"salgados\npão de queijo <3", "aval":"5" },	
+    {"id":"5","title":"Restaurante Universitário" , "subtitle":"refeições: café da manhã, almoço e janta!", "latitude":"-29.714113","longitude":"-53.718439" , "cardapio":"conferir no portal", "aval":"5" },	
 ],}
