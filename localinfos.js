c31e5c6874bcf874577342174c3c82093e3e052e
Caroline Siqueira <carolinesiqueira@iMac.local>
2016-12-13 14:12:57 -0200

    update js file

--- localinfos.js
+++ localinfos.js
@@ -1,12 +1,12 @@
 
 {"places":[
-    {"open":"1","title":"Lancheria do Menezes" , "subtitle":"lanches e almoço", "latitude":"-29.712860","longitude":"-53.716847" , "cardapio":"salgados gourmet\nsanduíches gourmet\ncoisas caras", "aval":"2" },
-    {"open":"0","title":"Posto da UFSM" , "subtitle":"conveniências", "latitude":"-29.711385","longitude":"-53.715995" , "cardapio":"salgados\ncachorro-quente\n","aval":"3" },
-    {"open":"1","title":"Xis do HUSM" , "subtitle":"lanches e almoços", "latitude":"-29.713931","longitude":"-53.714774" , "cardapio":"xis completo\nxis salada\n xis frango", "aval":"4" },
-    {"open":"0","title":"Lancheria do CCNE" , "subtitle":"lanches", "latitude":"-29.714625","longitude":"-53.716606" , "cardapio":"salgados\npão de queijo <3", "aval":"5" },	
-    {"open":"1","title":"Restaurante Universitário II" , "subtitle":"refeições: café da manhã, almoço e janta!", "latitude":"-29.714113","longitude":"-53.718439" , "cardapio":"conferir no portal da UFSM", "aval":"5" },
-    {"open":"0","title":"Restaurante Universitário I" , "subtitle":"refeições: café da manhã, almoço e janta!", "latitude":"-29.717011","longitude":"-53.714911" , "cardapio":"conferir no portal da UFSM", "aval":"4" },
-    {"open":"1","title":"Restaurante da Cléo" , "subtitle":"lanches e almoço", "latitude":"-29.722620","longitude":"-53.718108" , "cardapio":"Almoço\npasteis\nlanches", "aval":"4" },
-    {"open":"0","title":"La Loba" , "subtitle":"", "latitude":"-29.720020","longitude":"-53.718527" , "cardapio":"", "aval":"4" },
-    {"open":"1","title":"Pitadella" , "subtitle":"", "latitude":"-29.721418","longitude":"-53.714600" , "cardapio":"Almoço", "aval":"4" },
+    {"open":"1","title":"Lancheria do Menezes", "subtitle":"lanches e almoço", "latitude":"-29.712860","longitude":"-53.716847", "cardapio":"salgados gourmet\nsanduíches gourmet\ncoisas caras", "aval":"2", "image":"default" },
+    {"open":"0","title":"Posto da UFSM", "subtitle":"conveniências", "latitude":"-29.711385","longitude":"-53.715995", "cardapio":"salgados\ncachorro-quente\n","aval":"3", "image":"default" },
+    {"open":"1","title":"Xis do HUSM", "subtitle":"lanches e almoços", "latitude":"-29.713931","longitude":"-53.714774", "cardapio":"xis completo\nxis salada\n xis frango", "aval":"4", "image":"default" },
+    {"open":"0","title":"Lancheria do CCNE", "subtitle":"lanches", "latitude":"-29.714625","longitude":"-53.716606", "cardapio":"salgados\npão de queijo <3", "aval":"5", "image":"default" },	
+    {"open":"1","title":"Restaurante Universitário II", "subtitle":"refeições: café da manhã, almoço e janta!", "latitude":"-29.714113","longitude":"-53.718439", "cardapio":"conferir no portal da UFSM", "aval":"5", "image":"default" },
+    {"open":"0","title":"Restaurante Universitário I", "subtitle":"refeições: café da manhã, almoço e janta!", "latitude":"-29.717011","longitude":"-53.714911", "cardapio":"conferir no portal da UFSM", "aval":"4", "image":"default" },
+    {"open":"1","title":"Restaurante da Cléo", "subtitle":"lanches e almoço", "latitude":"-29.722620","longitude":"-53.718108", "cardapio":"Almoço\npasteis\nlanches", "aval":"4", "image":"default" },
+    {"open":"0","title":"La Loba", "subtitle":"", "latitude":"-29.720020","longitude":"-53.718527", "cardapio":"", "aval":"4", "image":"default" },
+    {"open":"1","title":"Pitadella", "subtitle":"", "latitude":"-29.721418","longitude":"-53.714600", "cardapio":"Almoço", "aval":"4", "image":"default" },
 ],}
